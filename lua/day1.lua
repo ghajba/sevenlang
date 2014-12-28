322bd7f2f51dbd1b132cd948f6a61a56fce7f038
Gabor <gabor.hajba@gmail.com>
2014-12-28 20:34:59 +0100

    is_prime

--- lua/day1.lua
+++ lua/day1.lua
@@ -1,4 +1,13 @@
 function ends_in_3(n)
-s = tostring(n)
+local s = tostring(n)
 return "3" == s:sub(#s,#s)
+end
+
+function is_prime(n)
+  if n <= 1 then return false
+  elseif n == 2 or n == 3 then return true end
+  for i=2, n/2 do
+    if n%i == 0 then return false end
+  end
+  return true
 end
